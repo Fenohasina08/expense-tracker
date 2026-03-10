a0754c5e8ac638943bb59d009e5abce62ee12852
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-10 20:58:21 +0300

    refactor: apply same responsive structure as dashboard to incomes page

--- frontend/src/Components/Site/Incomes.jsx
+++ frontend/src/Components/Site/Incomes.jsx
@@ -1,21 +1,16 @@
- import Header from "./Header";
+import Header from "./Header";
 import Sidebar from "./Sidebar";
 import ContentIncomes from "./ContentIncomes";
 
- 
 const Incomes = () => {
   return (
     <div className="flex flex-col h-screen w-full bg-gray-100">
-      <Header className="flex-none" />
-
-      <div className="flex flex-1 min-h-0">
-        <div className="w-64 flex-shrink-0">
-          <Sidebar />
-        </div>
-
-        <div className="flex-1 h-full overflow-auto p-2">
+      <Header />
+      <div className="flex flex-1 overflow-hidden pt-[10vh]">
+        <Sidebar />
+        <main className="flex-1 overflow-y-auto p-2 sm:p-4">
           <ContentIncomes />
-        </div>
+        </main>
       </div>
     </div>
   );
